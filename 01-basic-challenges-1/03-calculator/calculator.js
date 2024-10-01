e415e148c30d3c9db8a71b351fb59c984b3dbaf7
Angel Olvera <angelolveraramirez999@gmail.com>
2024-09-30 19:42:41 -0600

    Calculator challenge

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,24 @@
-function calculator() {}
+function calculator(a, b, operator) {
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;  
+        default:
+            throw new Error('Invalid Operator');
+    }
+
+    return result;
+}
 
 module.exports = calculator;
