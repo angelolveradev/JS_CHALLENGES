357d9efae1cc2b9aa9df2154c583dd3d954b4978
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-01 22:01:51 -0600

    Count Ocurences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,14 @@
-function countOccurrences() {}
+/*function countOccurrences(str, char) {
+    let cont = 0;
+    for (let i=0; i<str.length; i++){
+        if (str[i] === char) {
+            cont++;
+        }
+        
+    }
+    return cont;
+}*/
+
+const countOccurrences = (str, char) => str.split(char).length-1;
 
 module.exports = countOccurrences;
