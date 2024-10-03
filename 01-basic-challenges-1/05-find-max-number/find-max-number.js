d72cda80708743c14db4389f95de9b67189f1752
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-03 00:02:15 -0600

    Find max math.max

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,17 @@
-function findMaxNumber() {}
+// function findMaxNumber(array) {
+//     let max = array[0];
+//     for (let i=1; i<array.length; i++){
+//         if (max<array[i]) {
+//             max = array[i];
+//         }
+//     }
+//     return max;
+
+// }
+
+function findMaxNumber(arr) {
+    return Math.max(...array)
+}
+
 
 module.exports = findMaxNumber;
