29f01b95d7797b0ef4b5e57c8cd5738b4b69009e
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-03 13:31:05 -0600

    Palindrome

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -3,7 +3,11 @@
 // }
 
 function reverseString(str) {
-    return str.split('').reverse().join('');
+    let reversed = '';
+    for(let i = str.length-1; i>=0; i--){
+        reversed = reversed + str[i];
     }
+    return reversed;
+}
     
 module.exports = reverseString;
--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,23 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+    let cleanstr = '';
+    for (let i=0; i<str.length;i++){
+        let char = str[i].toLowerCase();
+        if ((char >= 'a' && char <= 'z') || (char >= '0' && char <= '9')) {
+            cleanstr += char;
+        }
+    }
+
+    let reversed = '';
+    for (let i=cleanstr.length-1;i>=0;i--){
+        reversed += cleanstr[i];
+    }
+
+    if (reversed === cleanstr){
+        return true;}
+        else {
+            return false;
+    }
+
+}
 
 module.exports = isPalindrome;
