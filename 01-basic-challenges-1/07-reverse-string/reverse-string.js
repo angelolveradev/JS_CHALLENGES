043e6e241159de938efe869743f9965a516bcf98
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-03 12:41:37 -0600

    Reverse String

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,9 @@
-function reverseString() {}
+// function reverseString(str) {
+// return str.split('').reverse().join('');
+// }
 
+function reverseString(str) {
+    return str.split('').reverse().join('');
+    }
+    
 module.exports = reverseString;
