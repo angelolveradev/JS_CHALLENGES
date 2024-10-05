aae1aa20742e5e361e0b80cb8ef881e008f6d591
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-04 19:49:22 -0600

    Remove duplicates double for

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,6 +1,6 @@
 function removeDuplicates(array) {
     let totalchar = array.length;
-    for(let i = 0; i<totalchar.length; i++){
+    for(let i = 0; i<totalchar; i++){
         for (let j = i+1; j<totalchar; j++){
             if (array[i] === array [j]){
                 array.splice(j,1);
