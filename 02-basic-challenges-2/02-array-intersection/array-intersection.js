c3c46b021d1a5f9e3f6a4dde5f2c1a97804741eb
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-05 17:30:41 -0600

    Array Intersection Includes()

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,12 +1,21 @@
-function arrayIntersection(arr1,arr2) {
-    let arrintersection = [];
-    for (let i = 0; i<arr1.length; i++)
-        for (let j = 0; j<arr2.length; j++){
-            if (arr1[i]===arr2[j]){
-                arrintersection.push(arr1[i]);
+// function arrayIntersection(arr1,arr2) {
+//     let arrintersection = [];
+//     for (let i = 0; i<arr1.length; i++)
+//         for (let j = 0; j<arr2.length; j++){
+//             if (arr1[i]===arr2[j]){
+//                 arrintersection.push(arr1[i]);
+//             }
+//         }
+//         return arrintersection;
+// } 
+
+    function arrayIntersection(arr1,arr2){
+        let arrintersection = [];
+        for (let i = 0; i<arr2.length; i++){
+            if (arr1.includes(arr2[i])){
+                arrintersection.push(arr2[i]);
             }
         }
         return arrintersection;
-} 
-
+    }
 module.exports = arrayIntersection;
