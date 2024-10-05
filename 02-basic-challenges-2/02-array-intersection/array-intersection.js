f56e0491d8e5074d37d2a402b9b5c0d69b54c2ff
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-05 17:27:23 -0600

    Array Intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,12 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1,arr2) {
+    let arrintersection = [];
+    for (let i = 0; i<arr1.length; i++)
+        for (let j = 0; j<arr2.length; j++){
+            if (arr1[i]===arr2[j]){
+                arrintersection.push(arr1[i]);
+            }
+        }
+        return arrintersection;
+} 
 
 module.exports = arrayIntersection;
