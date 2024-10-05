409cf7b3ebb74d53943eb1d6cf0b95a98fff19e1
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-04 19:07:01 -0600

    Count vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,12 @@
-function countVowels() {}
+function countVowels(str) {
+    let vowels = str.toLowerCase();
+    let count = 0;
+    for (let i=0; i<vowels.length;i++){
+        if (vowels[i] === 'a' || vowels[i] === 'e' || vowels[i] === 'i' || vowels[i] === 'o' || vowels[i] === 'u' ){
+            count++;
+        } 
+    }
+    return count;
+}
 
 module.exports = countVowels;
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,12 @@
-function removeDuplicates() {}
+function removeDuplicates(array) {
+    let totalchar = array.lenght-1;
+    for(let i = 0; i<totalchar.length; i++){
+        if(array[i] === array[totalchar]){
+            delete(array[i]);
+            totalchar--;
+        }
+    }
+    return array;
+}
 
 module.exports = removeDuplicates;
