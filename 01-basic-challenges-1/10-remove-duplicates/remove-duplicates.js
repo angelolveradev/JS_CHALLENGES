a491d50a97e1bf44946a11f961f2070443d37ce7
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-04 19:59:54 -0600

    Duplicates using includes()

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,16 +1,26 @@
-function removeDuplicates(array) {
-    let totalchar = array.length;
-    for(let i = 0; i<totalchar; i++){
-        for (let j = i+1; j<totalchar; j++){
-            if (array[i] === array [j]){
-                array.splice(j,1);
-                totalchar--;
-                j--;
+// function removeDuplicates(array) {
+//     let totalchar = array.length;
+//     for(let i = 0; i<totalchar; i++){
+//         for (let j = i+1; j<totalchar; j++){
+//             if (array[i] === array [j]){
+//                 array.splice(j,1);
+//                 totalchar--;
+//                 j--;
+//             }
+//         }
+//         }
+
+//     return array;
+// }
+
+    function removeDuplicates(array) {
+        let unique = [];
+        for (let i=0; i<array.length; i++) {
+            if (!unique.includes(array[i])){
+                unique.push(array[i])
             }
         }
-        }
-
-    return array;
-}
+        return unique;
+    }
 
 module.exports = removeDuplicates;
