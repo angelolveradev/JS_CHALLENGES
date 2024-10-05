5fba5afdbc32d8971b79dc0e991e8a94a33e603f
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-05 17:57:20 -0600

    Arrayintersection Set

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -9,13 +9,25 @@
 //         return arrintersection;
 // } 
 
+    // function arrayIntersection(arr1,arr2){
+    //     let arrintersection = [];
+    //     for (let i = 0; i<arr2.length; i++){
+    //         if (arr1.includes(arr2[i])){
+    //             arrintersection.push(arr2[i]);
+    //         }
+    //     }
+    //     return arrintersection;
+    // }
+
     function arrayIntersection(arr1,arr2){
-        let arrintersection = [];
-        for (let i = 0; i<arr2.length; i++){
-            if (arr1.includes(arr2[i])){
-                arrintersection.push(arr2[i]);
+        const arrintersection = [];
+        const set1= new Set(arr1);
+        for (elemento of arr2){
+            if (set1.has(elemento)){
+                arrintersection.push(elemento);
             }
         }
         return arrintersection;
     }
+    
 module.exports = arrayIntersection;
