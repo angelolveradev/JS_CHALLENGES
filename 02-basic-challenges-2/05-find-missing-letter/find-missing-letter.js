95b92ccdf59c01710c397f4301739a867dcacda7
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-08 20:46:54 -0600

    Finding missing letter

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -10,14 +10,15 @@
 // }
 
 function findMissingLetter(arr) {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const indexstart = alphabet.indexOf(arr[0])
+    const indexstart = arr[0].charCodeAt(0);
     
     for (let i =0; i<arr.length; i++){
-        if (alphabet[indexstart + i] != arr[i]){
-            return alphabet[indexstart + i];
+        if (arr[i].charCodeAt(0) != indexstart + i){
+            return String.fromCharCode(indexstart + i);
         }
     }
+
+    return '';
     }
 
 module.exports = findMissingLetter;
