152db3fda663eab19b0da4760e6b8abc651b544d
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-08 19:30:38 -0600

    Finding missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,22 @@
-function findMissingNumber() {}
+// function findMissingNumber(arr) {
+//     let n = arr.length + 1;
+//     let expected =  n* (n + 1) / 2
+//     let actual = arr.reduce(function(sum,num){
+//         return sum+num;
+//     },0)
+//     return expected - actual;
+
+// }
+
+function findMissingNumber(arr) {
+    let n = arr.length + 1;
+    let expected =  n* (n + 1) / 2
+    let actual = 0;
+    for (let i=0; i<arr.length; i++){
+        actual += arr[i];
+    }
+    return expected - actual;
+
+}
 
 module.exports = findMissingNumber;
