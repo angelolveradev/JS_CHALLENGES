18024b582076a7dcf97e186a6591b90beee8d8f5
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-08 19:54:31 -0600

    Find missing string

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,23 @@
-function findMissingLetter() {}
+// function findMissingLetter(arr) {
+// const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// const indexstart = alphabet.indexOf(arr[0])
+
+// for (let i =0; i<arr.length; i++){
+//     if (alphabet[indexstart + i] != arr[i]){
+//         return alphabet[indexstart + i];
+//     }
+// }
+// }
+
+function findMissingLetter(arr) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const indexstart = alphabet.indexOf(arr[0])
+    
+    for (let i =0; i<arr.length; i++){
+        if (alphabet[indexstart + i] != arr[i]){
+            return alphabet[indexstart + i];
+        }
+    }
+    }
 
 module.exports = findMissingLetter;
