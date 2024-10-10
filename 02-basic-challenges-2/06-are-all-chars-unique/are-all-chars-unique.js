182249732ed712b6ce8bed613de5878a1c097a6f
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-09 22:31:20 -0600

    Are all chars unique using set

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -9,16 +9,26 @@
 //         return true;
 //     }
     
-    function areAllCharactersUnique(str){
-        let unique = '';
-        for (char of str){
-            if (!unique.includes(char)){
-                unique +=char;
-            } else
-        return false;
-    }   
-    return true;
+    // function areAllCharactersUnique(str){
+    //     let unique = '';
+    //     for (char of str){
+    //         if (!unique.includes(char)){
+    //             unique +=char;
+    //         } else
+    //     return false;
+    // }   
+    // return true;
 
+        function areAllCharactersUnique(str){
+            let charSet = new Set();
 
-    }
+            for (char of str){
+                if (charSet.has(char)){
+                    return false;
+                }
+
+                charSet.add(char);
+            }
+            return true;
+        }
 module.exports = areAllCharactersUnique;
