d7e956cb31806aace0a7d7b7ddc44a7b4a73b038
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-09 22:27:13 -0600

    Are all chars unique using includes

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,24 @@
-function areAllCharactersUnique() {}
+// function areAllCharactersUnique(str) {
+//     for (let i=0; i<str.length;i++){
+//         for (let j=i+1; j<str.length; j++) {
+//             if (str[i]=== str[j]) {
+//                 return false;
+//             }
+//         } 
+//         }
+//         return true;
+//     }
+    
+    function areAllCharactersUnique(str){
+        let unique = '';
+        for (char of str){
+            if (!unique.includes(char)){
+                unique +=char;
+            } else
+        return false;
+    }   
+    return true;
 
+
+    }
 module.exports = areAllCharactersUnique;
