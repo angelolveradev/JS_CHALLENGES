ba95d11d28a99bc673475f7179298bb592a13e61
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-12 17:47:07 -0600

    a

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating-solution.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating-solution.js
@@ -14,7 +14,7 @@ function findFirstNonRepeatingCharacter(str) {
     }
   }
 
-  // If no non-repeating chaaracter iss found, return null
+  // If no non-repeating chaaracter isas found, return null
   return null;
 }
 
