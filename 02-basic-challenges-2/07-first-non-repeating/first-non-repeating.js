4f5ff319fe94b32aa14cf887118fc504a88f1b83
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-14 02:29:19 -0600

    Using Maps

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,12 +1,28 @@
+// function findFirstNonRepeatingCharacter(str) {
+//     const charObject = {}
+
+//     for (const char of str){
+//         charObject[char] = ((charObject[char] || 0) + 1);
+//     }
+
+//     for (const char of str){
+//         if (charObject[char] === 1){
+//             return char;
+//         }
+//     }
+//     return null;
+// }
+
+
 function findFirstNonRepeatingCharacter(str) {
-    const charObject = {}
+    const charObject = new Map();
 
     for (const char of str){
-        charObject[char] = ((charObject[char] || 0) + 1);
+        charObject.set(char ,(charObject.get(char) || 0 ) + 1)
     }
 
     for (const char of str){
-        if (charObject[char] === 1){
+        if (charObject.get(char) === 1){
             return char;
         }
     }
