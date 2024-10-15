cd8cac2cba265982673fc0b151ad663f6ea967c7
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-14 20:22:34 -0600

    Dice game and Phone Number

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,28 @@
-function diceGameSimulation() {}
+function rollDice(){
+    return Math.floor((Math.random() * 6) + 1);
+}
+
+function diceGameSimulation(numOfSimulations) {
+    const results = [];
+
+    for (let i = 0; i<numOfSimulations; i++){
+        let dice1 = rollDice();
+        let dice2 = rollDice();
+        let sum = dice1 + dice2;
+        let result = ''; 
+        
+        if (sum === 7 || sum === 11){
+            result = 'win';
+        } else if (sum === 2 || sum === 3 || sum === 12 ){
+            result = 'lose';
+        } else {
+            result = 'roll again';
+        }
+        
+        results.push ({dice1, dice2, sum, result});
+    }
+
+    return results;
+}
 
 module.exports = diceGameSimulation;
--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,9 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numbers) {
+    const codeArea = numbers.slice(0,3).join('');
+    const prefix = numbers.slice(3,6).join('');
+    const lineNumber = numbers.slice(6).join('');
+
+    return `(${codeArea}) ${prefix}-${lineNumber}  `
+}
 
 module.exports = formatPhoneNumber;
