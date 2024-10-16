d6aebaadf5d48053fb12b62012458efa95ec139b
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-16 01:30:27 -0600

    validate email

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,20 @@
-function validateEmail() {}
+function validateEmail(email) {
+
+    if (email.indexOf('@')=== -1) {
+        return false;
+    }
+
+    const [part1, part2] = email.split('@');
+    if (part1.length < 2 || part2.length < 3) {
+        return false;
+    }
+
+    const domain = part2.split('.');
+    if (domain.length < 2  || domain[1].length < 2){
+        return false;
+    }
+
+    return true;
+}
 
 module.exports = validateEmail;
