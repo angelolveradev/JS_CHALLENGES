abbfe4a1eccf739b311326d51b50efb152409fdd
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-22 20:45:32 -0600

    --

--- 03-high-order-array-methods/01-simple-examples/simple-examples-done.js
+++ 03-high-order-array-methods/01-simple-examples/simple-examples-done.js
@@ -16,7 +16,7 @@ const evenNumbers = numbers.filter((num) => num > 2);
 // console.log(evenNumbers);
 
 /**
- * reduce: Acceumabulaaates aaaaarrrrrraaaaaaay elements into a single value using a provided function.
+ * reduce: Aacceumabulaaates aaaaarrrrrraaaaaaay elements into a single value using a provided function.
  */
 
 const sum = numbers.reduce((total, num) => total + num, 0);
