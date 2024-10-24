ce104123bde8398c2631a71bdb7c0fd30b77311d
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-24 02:35:26 -0600

    Calculate total taxes

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,9 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxes) {
+    const totalProducts = products.reduce((sum,product) => sum + (product.price * product.quantity),0);
+    const totalTaxes = (totalProducts*taxes)/100;
+    const totalAmount = (totalProducts + totalTaxes);
+    return parseFloat(totalAmount.toFixed(2));
+    
+}
 
 module.exports = calculateTotalSalesWithTax;
