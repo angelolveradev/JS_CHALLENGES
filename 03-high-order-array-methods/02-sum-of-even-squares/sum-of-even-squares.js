af5d30a786a1221bd4367953f45ef2f6b4378d0b
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-24 02:15:21 -0600

    sum of even squares

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,10 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+    const evenNumbers = numbers
+    .filter((num) => num % 2 === 0)
+    .map((num) => num ** 2)
+    .reduce((sum,num) => sum + num, 0);
+
+    return evenNumbers;
+}
 
 module.exports = sumOfEvenSquares;
