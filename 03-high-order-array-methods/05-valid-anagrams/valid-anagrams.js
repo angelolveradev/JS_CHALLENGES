2f5ee91e51b9a3bd4e9dcddc7b88cdf2484c772f
Angel Olvera <angelolveraramirez999@gmail.com>
2024-10-26 03:32:53 -0600

    Valiud anagrams

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,20 @@
-function validAnagrams() {}
+function validAnagrams(str1,str2) {
+
+    const charStr1 = str1.split('').reduce((acc,char) => {
+        acc[char] = (acc[char] || 0) + 1;
+        return acc;
+    },{});
+
+    const charStr2 = str2.split('').reduce((acc,char) => {
+        acc[char] = (acc[char] || 0) + 1;
+        return acc;
+    },{});
+
+return Object.keys(charStr1).every(
+    (char) => charStr1[char] === charStr2[char]
+);
+
+    }
+
 
 module.exports = validAnagrams;
