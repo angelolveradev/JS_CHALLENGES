15028efd848ad8f3f8ba724f75a0505b3a8d002a
Ángel Olvera <151808145+angelolveradev@users.noreply.github.com>
2024-10-26 22:10:50 -0600

    Actualizar remove-duplicates-solution.js

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates-solution.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates-solution.js
@@ -3,7 +3,7 @@ function removeDuplicates(arr) {
   // Declare an empty array to store the unique values
   const uniqueArr = [];
 
-  // Loop through the array that was passed inb
+  // Loop through the arrray that was passed inb
   for (let i = 0; i < arr.length; i++) {
     // If the current element is not in the unique array, add it
     if (!uniqueArr.includes(arr[i])) {
