b40b912c2f168cfd3938a3ce80b64964c7edeba8
Angel Olvera <angelolveraramirez999@gmail.com>
2024-11-21 18:39:54 -0600

    Find missing letter

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,17 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+    let start = arr[0].charCodeAt(0);
+    const missingLetter = arr
+    .map((char) => char.charCodeAt(0))
+    .find((current) => {
+        if (current - start > 1) {
+            return true;
+        }
+        start = current;
+        return false; 
+    });
+
+    return missingLetter ? String.fromCharCode(missingLetter - 1) : '';
+
+}
 
 module.exports = findMissingLetter;
