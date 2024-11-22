d8e0bfbc0cb72617922e9815844579109aa4d751
Angel Olvera <angelolveraramirez999@gmail.com>
2024-11-21 18:14:01 -0600

    Password Validator

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,11 @@
-function validatePassword() {}
+function validatePassword(password) {
+    const hasValidLength = password.length >= 8;
+    const hasUppercase = password.split('').some((char) => char.toUpperCase() === char && char.toLowerCase() != char);
+    const hasLowercase = password.split('').some((char) => char.toLowerCase() === char && char.toUpperCase() != char);
+    const hasDigit = password.split('').some((char) => !isNaN(parseInt(char,10)));
+
+    return hasValidLength && hasUppercase && hasLowercase && hasDigit;
+
+}
 
 module.exports = validatePassword;
