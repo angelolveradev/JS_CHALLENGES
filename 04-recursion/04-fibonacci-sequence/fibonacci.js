e9f93792e5d86cb19f76ea8d767e8e0db8223f3a
Angel Olvera <angelolveraramirez999@gmail.com>
2024-11-22 00:53:33 -0600

    Fibonacci secuence

--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,3 +1,12 @@
-function fibonacci() {}
+function fibonacci(num) {
+    if(num ===  1) {
+        return num;
+    };
+    if(num ===  0) {
+        return num;
+    };
+
+    return fibonacci (num-1) + fibonacci (num-2);
+}
 
 module.exports = fibonacci;
