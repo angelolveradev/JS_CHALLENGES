da3904501c82183adec9405736c3433a17349157
Angel Olvera <angelolveraramirez999@gmail.com>
2024-11-23 21:54:11 -0600

    Time notation

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,12 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+
+    if(startNum === endNum) {
+        return [startNum];
+
+    };
+    const arr = numberRange(startNum, endNum -1);
+    arr.push(endNum);
+    return arr;
+}
 
 module.exports = numberRange;
